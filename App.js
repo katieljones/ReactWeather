3c1032f35db12147e56c1f8ae4fb57ce5a656d41
Katie Jones <katieljones29@gmail.com>
2020-10-19 11:52:21 +0100

    api request

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location'
-
+//not secure
+const WEATHER_API_KEY = '8227fa9634737d93ad1f03184cfb6e81'
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null)
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [unitsSystem, setUnitsSystem] = useState('metric')
   useEffect(() =>  {
     load()
   }, [])
@@ -17,25 +21,44 @@ export default function App() {
       }
       const location = await Location.getCurrentPositionAsync()
       const {latitude, longitude} = location.coords
-      alert(`Latitude : ${latitude}, Longitude : ${longitude}`)
-
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+      const response = await fetch(weatherUrl)
+      const result = await response.json()
+      if(response.ok) {
+        setCurrentWeather(result)
+      } else {
+        setErrorMessage(result.message)
+      }
     } catch (error) {
-
+      setErrorMessage(error.message)
     }
   }
+  if(currentWeather) {
+    const { main : {temp}} = currentWeather
   return (
     <View style={styles.container}>
-      <Text>Hey!</Text>
-      <StatusBar style="auto" />
+    <StatusBar style="auto" />
+    <View style={styles.main}>
+      <Text>{temp}</Text>
+
     </View>
-  );
+  )} else {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  main: {
+    justifyContent: 'center',
+    flex: 1
+  }
 });
